e34d0e0ef747c1acf6eebaacb467200e913f7109
zzhan133 <tmzhangzhe@gmail.com>
2017-05-17 10:18:53 -0700

    41. First Missing Positive

--- java/src/main/java/com/mv/leetCode/FirstMissingPositive.java
+++ java/src/main/java/com/mv/leetCode/FirstMissingPositive.java
@@ -2,47 +2,29 @@ package com.mv.leetCode;
 
 public class FirstMissingPositive {
 
-    public int firstMissingPositive(int[] nums) {
-    	if(nums == null || nums.length == 1 && nums[0] <= 0) {
-    		return 1;
-    	}
-    	int left = -1;
-    	int right = nums.length;
-    	while(left < right) {
-    		//1. left is empty -> nums.length;
-    		//2. left is bigger than 0;
-    		//nums[left] == nums[right];
-    		while(++left < right && nums[left] <= 0);
+	public int firstMissingPositive(int[] nums) {
+		if (nums == null || (nums.length == 1 && nums[0] != 1)) {
+			return 1;
+		}
+		for (int i = 0; i < nums.length; i++) {
+			nums[i] = nums[i] - 1;
+		}
+		for (int i = 0; i < nums.length; i++) {
+			int newValue = nums[i];
+			int backup = 0;
+			while (newValue >= 0 && newValue < nums.length && newValue != nums[newValue]) {
+				backup = nums[newValue];
+				nums[newValue] = newValue;
+				newValue = backup;
+			}
 
-    		
-    		//1. right == left;
-    		//2. nums[right] <= 0;
-    		while(--right > left && nums[right] > 0);
-    		
-    		// nums[left] is greater than 0;
-    		if(left < right) {
-        		int tmp = nums[left];
-        		nums[left] = nums[right];
-        		nums[right] = tmp;
-    		}
-    	}
-    	
-    	if(left == nums.length) return 1;
-    	
-    	//left is the first element greater than 0;
-    	int size = nums.length - left;
-    	int calSum = size * (size + 1) / 2;
-    	int sum = 0;
-    	while(left < nums.length) {
-    		sum += nums[left];
-    		left++;
-    	}
-    	
-    	if(sum == calSum) {
-    		return size + 1;
-    	}
-    	
-    	return calSum + size + 1 - sum;
-    }
+		}
+		for (int i = 0; i < nums.length; i++) {
+			if(i != nums[i]) {
+				return i + 1;
+			}
+		}
+		return nums.length + 1;
+	}
 
 }
--- java/src/test/java/com/mv/leetCode/FirstMissingPositiveTest.java
+++ java/src/test/java/com/mv/leetCode/FirstMissingPositiveTest.java
@@ -41,4 +41,22 @@ public class FirstMissingPositiveTest {
 		int[] nums = {1, 0};
 		assertEquals(2 ,obj.firstMissingPositive(nums));
 	}
+	
+	@Test
+	public void testFirstMissingPositive6() throws Exception {
+		int[] nums = {1, 1};
+		assertEquals(2 ,obj.firstMissingPositive(nums));
+	}
+	
+	@Test
+	public void testFirstMissingPositive7() throws Exception {
+		int[] nums = {2, 1};
+		assertEquals(3 ,obj.firstMissingPositive(nums));
+	}
+	
+	@Test
+	public void testFirstMissingPositive8() throws Exception {
+		int[] nums = {3, 2};
+		assertEquals(1 ,obj.firstMissingPositive(nums));
+	}
 }
